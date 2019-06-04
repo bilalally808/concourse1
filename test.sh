e770c92e0cab4f32a7f00c776059ddec5998c142
Bilal Ally <bilalally@Bilals-MBP.localdomain>
2019-06-04 16:40:38 +0100

    changing repo

--- test.sh
+++ test.sh
@@ -1,3 +1,18 @@
 #!/bin/sh
+FROM gliderlabs/alpine:3.3
 
-echo "hello"
+RUN apk add --update \
+    python \
+    python-dev \
+    py-pip \
+    build-base \
+  && pip install virtualenv \
+  && rm -rf /var/cache/apk/*
+
+WORKDIR /app
+
+ONBUILD COPY . /app
+ONBUILD RUN virtualenv /env && /env/bin/pip install -r /app/requirements.txt
+
+EXPOSE 8080
+CMD ["/env/bin/python", "main.py"]
